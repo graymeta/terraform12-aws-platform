a5f40e72b1b3b585fd0fc3c7a2d40c6fc5b7a371
Aron Rightious <aron.rightious@graymeta.com>
2020-08-04 07:31:12 -0700

    fmt

--- modules/elasticache/caches.tf
+++ modules/elasticache/caches.tf
@@ -5,7 +5,7 @@ resource "aws_elasticache_cluster" "services" {
   node_type            = var.instance_type_services
   port                 = 6379
   num_cache_nodes      = 1
-  security_group_ids = [var.elasticache_security_group]
+  security_group_ids   = [var.elasticache_security_group]
   subnet_group_name    = var.elasticache_subnet_group_name
   parameter_group_name = "default.redis3.2"
 }
